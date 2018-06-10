6e2de8d4d11021b51df94d1682dfa1e4ac48b67b
Chris <chrisabbod@gmail.com>
2018-06-10 12:59:17 -0400

    <feat>: Clicking on List Item Now Goes to USGS.gov Entry for that Item

--- app/src/main/java/com/example/android/quakereport/Earthquake.java
+++ app/src/main/java/com/example/android/quakereport/Earthquake.java
@@ -11,16 +11,23 @@ public class Earthquake {
     /** Date of the earthquake */
     private long mTimeInMilliseconds;
 
+    /** Website URL of the earthquake */
+    private String mUrl;
+
     /**
+     * Constructs a new {@link Earthquake} object.
      *
      * @param magnitude is the magnitude (size) of the earthquake
-     * @param location is the city location of the earthquake
-     * @param timeInMilliseconds is the date the earthquake happened
+     * @param location is the location where the earthquake happened
+     * @param timeInMilliseconds is the time in milliseconds (from the Epoch) when the
+     *                           earthquake happened
+     * @param url is the website URL to find more details about the earthquake
      */
-    public Earthquake(double magnitude, String location, long timeInMilliseconds){
+    public Earthquake(double magnitude, String location, long timeInMilliseconds, String url) {
         mMagnitude = magnitude;
         mLocation = location;
         mTimeInMilliseconds = timeInMilliseconds;
+        mUrl = url;
     }
 
     public double getMagnitude() {
@@ -45,5 +52,12 @@ public class Earthquake {
 
     public void setTimeInMilliseconds(long mTimeInMilliseconds) {
         this.mTimeInMilliseconds = mTimeInMilliseconds;
+    }
+
+    /**
+     * Returns the website URL to find more information about the earthquake.
+     */
+    public String getUrl() {
+        return mUrl;
     }
 }
